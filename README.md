13bdf64a88ebf3be2b92287eafe6009063b11f85
Juan Ceja <todoendgame@gmail.com>
2018-04-25 14:47:15 -0700

    Add project template to master with a README file

---
+++ README.md
@@ -0,0 +1,1 @@
+# cs100 team1 project, creating an android real time chat app
---
+++ app/src/main/java/team1/cs100/ucrchatapp/MainActivity.java
@@ -0,0 +1,13 @@
+package team1.cs100.ucrchatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
