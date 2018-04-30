ae4ea3842461b7aedd18a258a65683b0ce0c3136
Juan Ceja <todoendgame@gmail.com>
2018-04-30 06:12:34 -0700

    Add Register Activity and templates for Login and Start Activty
    I'm 85% complete with the register activity just testing for bugs.
    The login activty is very similiar to the Register activity. I
    will complete that today. I need to add a button in the start activity
    that lets the user login I have the create account button made already.

---
+++ app/src/main/java/team1/cs100/ucrchatapp/LoginActivity.java
@@ -0,0 +1,20 @@
+package team1.cs100.ucrchatapp;
+
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
--- app/src/main/java/team1/cs100/ucrchatapp/MainActivity.java
+++ app/src/main/java/team1/cs100/ucrchatapp/MainActivity.java
@@ -1,13 +1,68 @@
 package team1.cs100.ucrchatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    /*Firebase Authentication*/
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*initialize the FirebaseAuth instance*/
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+        //test
+        /*
+         * NOTE: To team members I need to include this because without it ounce your
+         * sign in you can't signed out because of the google "smartlock".
+         * When the sign out logic is implemented go ahead and remove this.
+         * Also this will skip the MainActivaity page becuase the user is never going tp be signed
+         * in. So if you are testing the registering and it keeps taking you back to start activity
+         * thats why.
+         */
+         mAuth.signOut();
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        /*
+         * ************************************************************************************
+         * Checking if current user is logged in if yes then perform and load chat logic and ui.
+         * Else move to StartPageActivty to sign in or register the current user
+         **************************************************************************************/
+
+        if (currentUser == null) {
+            //Start sign in/sign up process
+            Intent startActivityIntent = new Intent(MainActivity.this, StartActivty.class);
+            startActivity(startActivityIntent);
+            finish();
+
+        } else {
+            //user is already signed in. Therefor display a welcome toast
+            Toast.makeText(this, "Welcome Back!!!" + FirebaseAuth.getInstance()
+                    .getCurrentUser().getDisplayName(), Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
 }
