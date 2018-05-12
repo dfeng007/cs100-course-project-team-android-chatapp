9a2d8af76d9bb31a742c1448a276f4dea33414a7
Katherine Legaspi <klega002@ucr.edu>
2018-05-11 19:25:28 -0700

    Updated the UI for profileActivity

---
+++ app/src/main/java/chatapp/ucr/com/ucrapp/CustomizeActivity.java
@@ -0,0 +1,12 @@
+package chatapp.ucr.com.ucrapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CustomizeActivity extends AppCompatActivity {
+
+
+}
